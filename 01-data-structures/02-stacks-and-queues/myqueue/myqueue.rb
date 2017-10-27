74b2f89dd63de78986084f58ac733e442661325d
Carissa Castro <carissabcastro@gmail.com>
2017-10-27 11:04:46 -0500

    Stacks and Queues complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,36 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :queue
 
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[@queue.length - 1]
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue[0]
+    @tail = @queue[@queue.length - 1]
   end
 
   def dequeue
+    @head = @queue[1]
+    0.upto(@queue.length - 1) do |i|
+      if i == @queue.length - 1
+        @queue.delete_at(i)
+      else
+        @queue[i] = @queue[i+1]
+      end
+    end
   end
 
   def empty?
+    if @queue.length > 0
+      return false
+    else
+      return true
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,31 @@ class MyStack
   end
 
   def push(item)
+    @stack.length.downto(1) do |i|
+      @stack[i] = @stack[i - 1]
+    end
+    @stack[0] = item
+    self.top = @stack[0]
   end
 
   def pop
+    self.top = @stack[1]
+    temp = @stack[0]
+    0.upto(@stack.length - 1) do |i|
+      if i == @stack.length - 1
+        @stack[i] = nil
+      else
+        @stack[i] = @stack[i+1]
+      end
+    end
+    return temp
   end
 
   def empty?
+    if @stack.length > 0
+      return false
+    else
+      return true
+    end
   end
 end
