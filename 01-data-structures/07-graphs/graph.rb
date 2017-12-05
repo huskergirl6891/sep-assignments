137f9da2ae1d31d321a97a595fb52ef59c710b7a
Carissa Castro <carissabcastro@gmail.com>
2017-12-05 15:16:39 -0600

    Graphs not working

--- 01-data-structures/07-graphs/graph.rb
+++ 01-data-structures/07-graphs/graph.rb
@@ -12,16 +12,22 @@ class Graph
 
   def find_kevin_bacon(node_start)
     # Find the Baconator
+    # Check if node has been visited already
     if self.visited.include? node_start.name
+      puts node_start.name + " visited"
       return false
     end
+    # If not visited, add node to visited array
     self.visited.push(node_start.name)
+    # Check if node is Kevin Bacon
     if node_start.name == "Kevin Bacon"
       puts "found"
       return true
     end
+    # If node is not Kevin Bacon, use DFS to recursively search children of current node
     for film in node_start.film_actor_hash.keys do
       puts "outer loop = " + film
+      puts "outer loop node = " + node_start.name
       for child in node_start.film_actor_hash[film] do
         if find_kevin_bacon(child)
           puts "inner loop name = " + child.name
@@ -29,8 +35,10 @@ class Graph
           puts self.bacon_list
           puts "***********"
         end
+        return self.bacon_list
       end
     end
+    return nil
   end
 end
 
--- 01-data-structures/07-graphs/graph_spec.rb
+++ 01-data-structures/07-graphs/graph_spec.rb
@@ -20,10 +20,16 @@ RSpec.describe Graph, type: Class do
     kevin_bacon.film_actor_hash["Footloose"] = [lori_singer, john_lithgow, dianne_wiest]
     kevin_bacon.film_actor_hash["Apollo 13"] = [tom_hanks, bill_paxton, ed_harris]
     lori_singer.film_actor_hash["Footloose"] = [john_lithgow, dianne_wiest, kevin_bacon]
+    john_lithgow.film_actor_hash["Footloose"] = [lori_singer, dianne_wiest, kevin_bacon]
+    dianne_wiest.film_actor_hash["Footloose"] = [lori_singer, john_lithgow, kevin_bacon]
     ed_harris.film_actor_hash["Apollo 13"] = [tom_hanks, bill_paxton, kevin_bacon]
     ed_harris.film_actor_hash["A Beautiful Mind"] = [russell_crowe, jennifer_connelly]
+    tom_hanks.film_actor_hash["Apollo 13"] = [ed_harris, bill_paxton, kevin_bacon]
+    bill_paxton.film_actor_hash["Apollo 13"] = [ed_harris, tom_hanks, kevin_bacon]
     russell_crowe.film_actor_hash["A Beautiful Mind"] = [ed_harris, jennifer_connelly]
+    jennifer_connelly.film_actor_hash["A Beautiful Mind"] = [ed_harris, russell_crowe]
     christian_bale.film_actor_hash["The Dark Knight"] = [heath_ledger]
+    heath_ledger.film_actor_hash["The Dark Knight"] = [christian_bale]
   end
 
   describe "#find_kevin_bacon(data)" do
@@ -32,7 +38,7 @@ RSpec.describe Graph, type: Class do
     end
 
     it "finds kevin bacon when actor is two films away" do
-      expect(film_graph.find_kevin_bacon(russell_crowe)).to eq ["A Beautiful Mind", "Apollo 13"]
+      expect(film_graph.find_kevin_bacon(russell_crowe)).to eq ["Apollo 13", "A Beautiful Mind"]
     end
 
     it "finds nil when actor is not tied to Kevin Bacon" do
