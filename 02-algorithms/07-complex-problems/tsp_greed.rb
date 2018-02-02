104be0f9e13f50b15b3bd7c287daf8d8ad074982
Carissa Castro <carissabcastro@gmail.com>
2018-02-02 09:48:19 -0600

    Checkpoint 7 complete

--- 02-algorithms/07-complex-problems/tsp_greed.rb
+++ 02-algorithms/07-complex-problems/tsp_greed.rb
@@ -5,7 +5,7 @@ class Graph
   attr_accessor :city_list
 
   def initialize
-    self.visited = []
+    #self.cities_visited = []
     self.city_list = []
   end
 
@@ -13,24 +13,21 @@ class Graph
   def nearest_neighbor(current_city)
     count = 0
     while current_city.visited == false do
-      #puts current_city.name
       neighbor_cities = current_city.neighbors
-      #puts neighbor_cities
       next_city = neighbor_cities[0][0]
       distance = neighbor_cities[0][1]
-      for current_neighbor in neighbor_cities do
-        if current_neighbor[1] < distance
+
+      neighbor_cities.each_with_index do |current_neighbor, index|
+        if next_city.visited == true || current_neighbor[0].visited == false && current_neighbor[1] < distance
           next_city = current_neighbor[0]
         end
       end
-      puts next_city.name
       self.city_list.push(current_city.name)
       current_city.visited = true
       current_city = next_city
-      #puts current_city.name
       count += 1
-      puts count
     end
+    print self.city_list
     return self.city_list
   end
 end
